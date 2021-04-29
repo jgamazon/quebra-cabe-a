48d2a57a25a395d4799b5ea23917875b7b63d704
Járlesson Júnior <jgamazonjr@gmail.com>
2021-04-28 22:54:05 -0300

    puzzle tradicional + menu
    Adicionei o quebra-cabeça tradicional e parte do menu tá configurado

---
+++ Assets/scripts/DragAndDrop.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class DragAndDrop : MonoBehaviour
+{
+    public GameObject SelectedPiece;
+    int OIL = 1;
+   
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.GetMouseButtonDown(0)) {
+             
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.transform.CompareTag("Puzzle")){
+                if (!hit.transform.GetComponent<PuzzlePiece>().InRightPosition) {
+                    SelectedPiece = hit.transform.gameObject;
+                    SelectedPiece.GetComponent<PuzzlePiece>().Selected = true;
+                    SelectedPiece.GetComponent<SortingGroup>().sortingOrder = OIL;
+                    OIL++;
+                }
+            
+            }
+        }
+        if (Input.GetMouseButtonUp(0)){
+            
+            if (SelectedPiece != null) {
+            SelectedPiece.GetComponent<PuzzlePiece>().Selected = false;
+            SelectedPiece = null;}
+        }
+        if (SelectedPiece != null){
+            Vector3 MousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            SelectedPiece.transform.position = new Vector3 (MousePoint.x, MousePoint.y, 0);
+        } 
+    }
+}
---
+++ Assets/scripts/PuzzlePiece.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+public class PuzzlePiece : MonoBehaviour
+{
+    private Vector3 RightPosition;
+    public bool InRightPosition;
+    public bool Selected;
+
+    void Start()
+    {
+        RightPosition = transform.position;
+        ///////// assim que o jogo comeca
+        ///////// as pecas ficarao dispostas em posicoes aleatorias
+        transform.position = new Vector3(Random.Range(1f, 8.4f), Random.Range(3.2f, -3.2f));
+    }
+ 
+    void Update()
+    {
+        // caso o jogador chegue com a peca perto da sua posicao de origem
+        // ela vai encaixar automaticamente
+        if (Vector3.Distance(transform.position, RightPosition) < 0.8f) {
+            if(!Selected) {
+            if(InRightPosition == false) {
+            transform.position = RightPosition;
+            // quando encaixada, ela nao pode ser selecionada
+            InRightPosition = true;
+            GetComponent<SortingGroup>().sortingOrder = 0;
+            }
+            }}
+        }
+    }
+    public class ScaleAndShake : MonoBehaviour {
+        
+}
